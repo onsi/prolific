6682f94f4be55a48a676d67816d39902c30d56f6
Mike Dalessio <mike.dalessio@gmail.com>
2015-02-12 20:51:26 -0500

    Introduce a Describe context for reading from file
    in preparation for introducing "reading from stdin"

--- prolific_test.go
+++ prolific_test.go
@@ -63,45 +63,49 @@ var _ = Describe("Prolific", func() {
 			session, err = gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 			Ω(err).ShouldNot(HaveOccurred())
 			Eventually(session).Should(gexec.Exit(0))
-
-			cmd = exec.Command(prolific, "stories.prolific")
-			session, err = gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
-			Ω(err).ShouldNot(HaveOccurred())
-			Eventually(session).Should(gexec.Exit(0))
 		})
 
-		It("should convert the passed-in prolific file", func() {
-			reader := csv.NewReader(bytes.NewReader(session.Out.Contents()))
-			records, err := reader.ReadAll()
-			Ω(err).ShouldNot(HaveOccurred())
+		Describe("reading from file", func() {
+			BeforeEach(func() {
+				cmd := exec.Command(prolific, "stories.prolific")
+				session, err = gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+				Ω(err).ShouldNot(HaveOccurred())
+				Eventually(session).Should(gexec.Exit(0))
+			})
 
-			By("emitting a header line")
-			Ω(records[0]).Should(Equal([]string{"Title", "Type", "Description", "Labels"}))
+			It("should convert the passed-in prolific file", func() {
+				reader := csv.NewReader(bytes.NewReader(session.Out.Contents()))
+				records, err := reader.ReadAll()
+				Ω(err).ShouldNot(HaveOccurred())
 
-			By("parsing all entries")
-			Ω(records).Should(HaveLen(7))
+				By("emitting a header line")
+				Ω(records[0]).Should(Equal([]string{"Title", "Type", "Description", "Labels"}))
 
-			var TITLE, TYPE, DESCRIPTION, LABELS = 0, 1, 2, 3
+				By("parsing all entries")
+				Ω(records).Should(HaveLen(7))
 
-			By("parsing all relevant fields")
-			Ω(records[1][TITLE]).Should(Equal("As a user I can toast a bagel"))
-			Ω(records[1][TYPE]).Should(Equal("feature"))
-			Ω(records[1][DESCRIPTION]).Should(Equal("When I insert a bagel into toaster and press the on button, I should get a toasted bagel"))
-			Ω(records[1][LABELS]).Should(Equal("mvp,toasting"))
+				var TITLE, TYPE, DESCRIPTION, LABELS = 0, 1, 2, 3
 
-			By("handling types correctly")
-			Ω(records[3][TYPE]).Should(Equal("feature"))
-			Ω(records[4][TYPE]).Should(Equal("bug"))
-			Ω(records[5][TYPE]).Should(Equal("chore"))
-			Ω(records[6][TYPE]).Should(Equal("release"))
+				By("parsing all relevant fields")
+				Ω(records[1][TITLE]).Should(Equal("As a user I can toast a bagel"))
+				Ω(records[1][TYPE]).Should(Equal("feature"))
+				Ω(records[1][DESCRIPTION]).Should(Equal("When I insert a bagel into toaster and press the on button, I should get a toasted bagel"))
+				Ω(records[1][LABELS]).Should(Equal("mvp,toasting"))
 
-			By("handling empty descriptions correctly")
-			Ω(records[4][DESCRIPTION]).Should(BeEmpty())
+				By("handling types correctly")
+				Ω(records[3][TYPE]).Should(Equal("feature"))
+				Ω(records[4][TYPE]).Should(Equal("bug"))
+				Ω(records[5][TYPE]).Should(Equal("chore"))
+				Ω(records[6][TYPE]).Should(Equal("release"))
 
-			By("handling labels correctly")
-			Ω(records[3][LABELS]).Should(Equal("mvp,clean-up"))
-			Ω(records[5][LABELS]).Should(BeEmpty())
-			Ω(records[6][LABELS]).Should(Equal("mvp"))
+				By("handling empty descriptions correctly")
+				Ω(records[4][DESCRIPTION]).Should(BeEmpty())
+
+				By("handling labels correctly")
+				Ω(records[3][LABELS]).Should(Equal("mvp,clean-up"))
+				Ω(records[5][LABELS]).Should(BeEmpty())
+				Ω(records[6][LABELS]).Should(Equal("mvp"))
+			})
 		})
 	})
 })
